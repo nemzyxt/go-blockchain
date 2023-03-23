95d4ba510b66b055112609b8022c1f0722ccf628
Nemuel Wainaina <bR0S3@protonmail.com>
2023-03-23 19:24:58 +0300

    Define function to derive hash for a block

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
 )
 
@@ -8,6 +10,12 @@ type Block struct {
 	Hash []byte
 	Data []byte
 	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
 }
 
 func main() {
