6aad11e20c0d8d1af00288a74439afb4b23cb063
Nemuel Wainaina <bR0S3@protonmail.com>
2023-03-23 19:43:04 +0300

    Implement the blockchain in main function

--- main.go
+++ main.go
@@ -43,5 +43,15 @@ func InitBlockChain() *BlockChain {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	chain := InitBlockChain()
+
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous Hash : %x\n", block.PrevHash)
+		fmt.Printf("Data in Block : %s\n", block.Data)
+		fmt.Printf("Hash : %x\n", block.Hash)
+	}
 }
