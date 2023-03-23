67eacb67a6b65cc41a24d8319c0e94ff7d231714
Nemuel Wainaina <bR0S3@protonmail.com>
2023-03-23 20:13:48 +0300

    Add the logic

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,1 +1,10 @@
 package blockchain
+
+/*
+ * Take data from the block
+ * Create a counter (nonce) which starts from 0
+ * Generate a hash using the data and the nonce
+ * Check whether the hash meets certain requirements
+ * Requirements :
+	- The hash has to contain 0s
+ */
